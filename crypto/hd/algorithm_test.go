0c663d714def5aaf48e0c5c9a85dbf6f43dedb15
Christian Borst <christianoborst@gmail.com>
2022-06-06 15:44:26 -0600

    Alter algo test to test eth_secp256k1/secp256k1 compatibility

--- crypto/hd/algorithm_test.go
+++ crypto/hd/algorithm_test.go
@@ -1,6 +1,8 @@
 package hd
 
 import (
+	"fmt"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"strings"
 	"testing"
 
@@ -11,8 +13,8 @@ import (
 	hdwallet "github.com/miguelmota/go-ethereum-hdwallet"
 
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/crypto/hd"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
-
 	cryptocodec "github.com/tharsis/ethermint/crypto/codec"
 	ethermint "github.com/tharsis/ethermint/types"
 )
@@ -25,24 +27,44 @@ func init() {
 const mnemonic = "picnic rent average infant boat squirrel federal assault mercy purity very motor fossil wheel verify upset box fresh horse vivid copy predict square regret"
 
 func TestKeyring(t *testing.T) {
+	config := sdk.GetConfig()
+	config.SetBech32PrefixForAccount("evmos", "evmospub")
+	// An account I just generated with 0 funds
+	compat_evmos_bech32 := "evmos15jc7t37ek7c6lqhjyjqxjca2vy6wel3jnzcw9w"
+	compat_mnemonic := "practice live method adult weather bean use sock proof illness swear short noble witness rebuild cook library crystal tone frame sadness elegant cement goose"
+
 	dir := t.TempDir()
 	mockIn := strings.NewReader("")
 
-	kr, err := keyring.New("ethermint", keyring.BackendTest, dir, mockIn, EthSecp256k1Option())
+	evmkr, err := keyring.New("evmos", keyring.BackendTest, dir, mockIn, EthSecp256k1Option())
+	gravkr, err := keyring.New("gravity", keyring.BackendMemory, dir, mockIn, EthSecp256k1Option())
 	require.NoError(t, err)
 
 	// fail in retrieving key
-	info, err := kr.Key("foo")
+	info, err := evmkr.Key("foo")
 	require.Error(t, err)
 	require.Nil(t, info)
 
 	mockIn.Reset("password\npassword\n")
-	info, mnemonic, err := kr.NewMnemonic("foo", keyring.English, ethermint.BIP44HDPath, keyring.DefaultBIP39Passphrase, EthSecp256k1)
+	info, err = evmkr.NewAccount("foo", compat_mnemonic, keyring.DefaultBIP39Passphrase, ethermint.BIP44HDPath, EthSecp256k1)
 	require.NoError(t, err)
-	require.NotEmpty(t, mnemonic)
+	require.NotEmpty(t, compat_mnemonic)
 	require.Equal(t, "foo", info.GetName())
 	require.Equal(t, "local", info.GetType().String())
 	require.Equal(t, EthSecp256k1Type, info.GetAlgo())
+
+	//defaultCosmosHDPath := hd.CreateHDPath(sdk.CoinType, 0, 0).String()
+	//ginfo, gmnem, gerr := gravkr.NewMnemonic("bar", keyring.English, ethermint.BIP44HDPath, keyring.DefaultBIP39Passphrase, hd.Secp256k1)
+	gmnem := compat_mnemonic
+	ginfo, gerr := gravkr.NewAccount("foo", compat_mnemonic, keyring.DefaultBIP39Passphrase, ethermint.BIP44HDPath, hd.Secp256k1)
+	require.NoError(t, gerr)
+	require.NotEmpty(t, gmnem)
+	require.Equal(t, "foo", ginfo.GetName())
+	require.Equal(t, "local", ginfo.GetType().String())
+	require.Equal(t, hd.Secp256k1Type, ginfo.GetAlgo())
+
+	fmt.Println("Mnemonic used", compat_mnemonic)
+	fmt.Println("Original address:", compat_evmos_bech32, "and addresses are ", info.GetAddress().String(), "and", ginfo.GetAddress().String())
 
 	hdPath := ethermint.BIP44HDPath
 
@@ -54,8 +76,8 @@ func TestKeyring(t *testing.T) {
 	require.Error(t, err)
 	require.Empty(t, wrongBz)
 
-	privkey := EthSecp256k1.Generate()(bz)
-	addr := common.BytesToAddress(privkey.PubKey().Address().Bytes())
+	evmPrivkey := EthSecp256k1.Generate()(bz)
+	evmAddr := common.BytesToAddress(evmPrivkey.PubKey().Address().Bytes())
 
 	wallet, err := hdwallet.NewFromMnemonic(mnemonic)
 	require.NoError(t, err)
@@ -64,7 +86,7 @@ func TestKeyring(t *testing.T) {
 
 	account, err := wallet.Derive(path, false)
 	require.NoError(t, err)
-	require.Equal(t, addr.String(), account.Address.String())
+	require.Equal(t, evmAddr.String(), account.Address.String())
 }
 
 func TestDerivation(t *testing.T) {
